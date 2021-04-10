a3f752c008094bc4cf9df21554c4c6d9eb9d556f
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-04-10 12:33:54 -0700

    animesingle poster added

--- components/Anime Posters/AnimePoster.tsx
+++ components/Anime Posters/AnimePoster.tsx
@@ -1,38 +1,80 @@
-import { Center, Grid, GridItem, Image } from "@chakra-ui/react";
-
+import { Center, Grid, GridItem } from "@chakra-ui/react";
+import Image from "next/image";
+import AnimeSinglePoster from "./AnimeSinglePoster";
 const AnimePoster = () => {
   return (
     <>
       <GridItem>
-        <Center>
-          <Grid
-            templateColumns="repeat(auto-fit, minmax(80px,1fr))"
-            gridAutoRows="90px"
-            gridAutoFlow="dense"
-          >
-            <GridItem>
-              <Image src="/AnimePosterImages/007.jfif"></Image>
-            </GridItem>
-            <GridItem>
-              <Image src="/AnimePosterImages/006.jfif"></Image>
-            </GridItem>
-            <GridItem>
-              <Image src="/AnimePosterImages/005.jfif"></Image>
-            </GridItem>
-            <GridItem>
-              <Image src="/AnimePosterImages/004.jfif"></Image>
-            </GridItem>
-            <GridItem>
-              <Image src="/AnimePosterImages/003.jfif"></Image>
-            </GridItem>
-            <GridItem>
-              <Image src="/AnimePosterImages/002.jfif"></Image>
-            </GridItem>
-            <GridItem>
-              <Image src="/AnimePosterImages/001.jfif"></Image>
-            </GridItem>
-          </Grid>
-        </Center>
+        <Grid
+          gap={1}
+          templateColumns="repeat(auto-fill,minmax(180px,1fr))"
+          autoRows="minmax(150px, auto)"
+          gridAutoFlow="dense"
+        >
+          <AnimeSinglePoster
+            alternateText="Horimiya Poster 720x1920 pixels"
+            source="/AnimePosterImages/007.jfif"
+            height="1280"
+            width="720"
+            tag1="Horimiya"
+            tag2="Anime"
+            date="April 10th, 2021"
+          />
+          <AnimeSinglePoster
+            alternateText="Horimiya Poster 720x1920 pixels"
+            source="/AnimePosterImages/006.jfif"
+            height="1280"
+            width="720"
+            tag1="Horimiya"
+            tag2="Anime"
+            date="April 10th, 2021"
+          />
+          <AnimeSinglePoster
+            alternateText="Horimiya Poster 720x1920 pixels"
+            source="/AnimePosterImages/005.jfif"
+            height="1280"
+            width="1280"
+            tag1="Horimiya"
+            tag2="Anime"
+            date="April 10th, 2021"
+          />
+          <AnimeSinglePoster
+            alternateText="Horimiya Poster 720x1920 pixels"
+            source="/AnimePosterImages/004.jfif"
+            height="1280"
+            width="720"
+            tag1="Horimiya"
+            tag2="Anime"
+            date="April 10th, 2021"
+          />
+          <AnimeSinglePoster
+            alternateText="Horimiya Poster 720x1920 pixels"
+            source="/AnimePosterImages/003.jfif"
+            height="1280"
+            width="720"
+            tag1="Horimiya"
+            tag2="Anime"
+            date="April 10th, 2021"
+          />
+          <AnimeSinglePoster
+            alternateText="Horimiya Poster 720x1920 pixels"
+            source="/AnimePosterImages/002.jfif"
+            height="1280"
+            width="720"
+            tag1="Horimiya"
+            tag2="Anime"
+            date="April 10th, 2021"
+          />
+          <AnimeSinglePoster
+            alternateText="Horimiya Poster 720x1920 pixels"
+            source="/AnimePosterImages/001.jfif"
+            height="1280"
+            width="720"
+            tag1="Horimiya"
+            tag2="Anime"
+            date="April 10th, 2021"
+          />
+        </Grid>
       </GridItem>
     </>
   );
---
+++ components/Anime Posters/AnimeSinglePoster.tsx
@@ -0,0 +1,77 @@
+import { Box, Badge, GridItem } from "@chakra-ui/react";
+import Image from "next/image";
+const AnimeSinglePoster = ({
+  source,
+  alternateText,
+  height,
+  width,
+  tag1,
+  tag2,
+  date,
+}) => {
+  const boxProperty = {
+    maxWidth: [130, 250, 350],
+    padding: [3, 5, 7],
+  };
+  return (
+    <>
+      <GridItem>
+        <Box
+          maxW={boxProperty.maxWidth}
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+        >
+          <Image
+            height={height}
+            width={width}
+            alt={alternateText}
+            src={source}
+          />
+
+          <Box p={boxProperty.padding}>
+            <Box d="flex" alignItems="baseline">
+              <Badge borderRadius="full" px="2" colorScheme="teal">
+                New
+              </Badge>
+              <Box
+                color="var(--secondary-text-color)"
+                fontWeight="semibold"
+                letterSpacing="wide"
+                fontSize="xs"
+                textTransform="uppercase"
+                ml="2"
+              >
+                {tag1}, {tag2}
+              </Box>
+            </Box>
+
+            <Box
+              mt="1"
+              fontWeight="semibold"
+              as="h4"
+              lineHeight="tight"
+              isTruncated
+            >
+              {alternateText}
+            </Box>
+
+            <Box>
+              <Box as="span" color="var(--main-text-color)" fontSize="sm">
+                10$
+              </Box>
+            </Box>
+
+            <Box d="flex" mt="2" alignItems="center">
+              <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                {date}
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </GridItem>
+    </>
+  );
+};
+
+export default AnimeSinglePoster;
