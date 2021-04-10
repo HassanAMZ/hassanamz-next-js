01102fd68bcd4956cb2945664ddb1c1bf0f91fc6
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-04-10 04:12:34 -0700

    env.local

--- pages/api/youtube.tsx
+++ pages/api/youtube.tsx
@@ -1,11 +1,14 @@
 export default async (req, res) => {
-  var key = process.env.YOUTUBE_KEY
-  const response = await fetch('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCfB_dBMG9TK2RlPsJdSXYSw&key=' + key)
-  const json = await response.json()
-  const { subscriberCount, viewCount, videoCount } = json.items[0].statistics
+  var key = process.env.local.YOUTUBE_KEY;
+  const response = await fetch(
+    "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCfB_dBMG9TK2RlPsJdSXYSw&key=" +
+      key
+  );
+  const json = await response.json();
+  const { subscriberCount, viewCount, videoCount } = json.items[0].statistics;
   return res.status(200).json({
-      subscriberCount,
-      viewCount,
-      videoCount
-  })
-}
+    subscriberCount,
+    viewCount,
+    videoCount,
+  });
+};
