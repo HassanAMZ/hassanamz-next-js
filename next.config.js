9047f4552574fd5fa56234e48dfb40a228bba7b4
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-04-27 06:43:51 +0500

    fixed, sitemap vercel error, next-config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports={
-    env:{
-        NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID:process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
-    },
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-          require('./scripts/generate-sitemap');
-        }
-    
-        return config;
-      }
+  env:{
+      NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID:process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
+  } 
 }
